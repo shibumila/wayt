a28fa70ddb17e98152abc0fb9c39d226bdebb948
Joseph Lyons <joseph.lyons@gmail.com>
2022-08-18 13:41:41 -0700

    TailWindTest

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello Greatest world EVER!</div>
+  return (
+    <div class="flex items-center justify-center h-screen">
+    
+    <div class="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+      Hello World
+    </div>
+    
+    </div>
+
+
+
+  )
+  
 }
